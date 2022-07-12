6c30a2ff60b566d87be5f0d5167d92045b8e12f4
Michael Primavera <pastasauce59@gmail.com>
2022-07-12 18:31:51 +0200

    changed route response as test, installed passportJS, set up use of passport

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys')
+
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send({ its: "ya boi" })
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL : '/auth/google/callback'
+}, (accessToken) => {
+    console.log(accessToken);
 })
+);
+
+// app.get('/', (req, res) => {
+//     res.send({ its: "ya boi" })
+// })
+
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
