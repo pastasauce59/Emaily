8836c76427a48373b3e8f5e79ead59cd3ac2b631
Michael Primavera <pastasauce59@gmail.com>
2022-07-13 16:17:39 +0200

    installed nodemon and added script to package.json

--- index.js
+++ index.js
@@ -9,8 +9,10 @@ passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
     callbackURL : '/auth/google/callback'
-}, (accessToken) => {
-    console.log(accessToken);
+}, (accessToken, refreshToken, profile, done) => {
+    console.log('access token', accessToken);
+    console.log('refresh token', refreshToken)
+    console.log('profile:', profile)
 })
 );
 
