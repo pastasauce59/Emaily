1949d8274e347f2cadc5a6fcb608be80976d24d6
Michael Primavera <pastasauce59@gmail.com>
2022-07-21 23:33:14 +0200

    testing authReducer and return values

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -5,7 +5,8 @@ import { FETCH_USER } from './types'
 export const fetchUser = () => async dispatch => {
        const res = await axios.get('/api/current_user')
         
-       dispatch({type: FETCH_USER, payload: res})
+       // res is the response from the axios.get() request
+       dispatch({type: FETCH_USER, payload: res.data })
 }
 
 // Older code pre-refactor below
