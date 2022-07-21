6c359828f78bb9f6b4b4115a3edcd42e2a42f379
Michael Primavera <pastasauce59@gmail.com>
2022-07-21 23:11:32 +0200

    FetchUser action refactor with async/await

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,9 +1,18 @@
 import axios from 'axios'
 import { FETCH_USER } from './types'
 
-export const fetchUser = () => {
-    return function(dispatch) {
-        axios.get('/api/current_user')
-        .then(res => dispatch({type: FETCH_USER, payload: res}))
-    }
+// async await refactor
+export const fetchUser = () => async dispatch => {
+       const res = await axios.get('/api/current_user')
+        
+       dispatch({type: FETCH_USER, payload: res})
 }
+
+// Older code pre-refactor below
+
+// export const fetchUser = () => {
+//     return function(dispatch) {
+//         axios.get('/api/current_user')
+//         .then(res => dispatch({type: FETCH_USER, payload: res}))
+//     }
+// }
