6ba6ee22e21ee4a1fca8635e347aeae9cace99d7
Michael Primavera <pastasauce59@gmail.com>
2022-07-22 01:09:56 +0200

    accessing state in Header and showing correct text when user is signed in or not

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,31 @@
+import { startSession } from 'mongoose';
 import React, { useState } from 'react'
+import { useSelector } from 'react-redux'
 
-function Header() {
+function Header(props) {
+
+    const value = useSelector(state => state)
+    // console.log(value)
+
+    let renderContent = () => {
+        switch (value.auth) {
+            case null:
+                return;
+            case false:
+                return (
+                    <li>
+                        <a href='/auth/google'>Login With Google</a>
+                    </li>
+                );
+            default:
+                return (
+                    <li>
+                        <a>Logout</a>
+                    </li>
+                );
+        }
+    }
+
     return (
         <nav>
             <div className='nav-wrapper'>
@@ -8,9 +33,10 @@ function Header() {
                     Emaily
                 </a>
                 <ul className='right'>
-                    <li>
+                    {renderContent()}
+                    {/* <li>
                         <a>Login With Google</a>
-                    </li>
+                    </li> */}
                 </ul>
             </div>
         </nav>
