0d1d25f89a12a1de86e1cedf947792e13441c3c3
Michael Primavera <pastasauce59@gmail.com>
2022-07-23 20:24:53 +0200

    fixed billingRoutes require statement (forgot to include app and it wasnt working), setting up billingRoutes to work with stripe charge object

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use(passport.initialize())
 app.use(passport.session())
 
 require('./routes/authRoutes')(app);
-require('./routes/billingRoutes')
+require('./routes/billingRoutes')(app)
 
 // app.get('/', (req, res) => {
 //     res.send({ its: "ya boi" })
