db62c021cd5c4cfa80ab8d37340c623ae128c42c
Michael Primavera <pastasauce59@gmail.com>
2022-07-26 15:04:02 +0200

    added production build paths and configurations

--- index.js
+++ index.js
@@ -36,5 +36,17 @@ require('./routes/billingRoutes')(app)
 //     res.send({ its: "ya boi" })
 // })
 
+if (process.env.NODE_ENV === 'production'){ 
+    // Express will serve up production assets
+    // like our main.js file, or main.css file
+    app.use(express.static('client/build'))
+
+    //Express will serve up index.html file if it doesn't recognize the route
+    const path = require('path')
+    app.get('*',(req, res) => {
+        res.sendFile(path.resolve__dirname, 'client', 'build','index.html')
+    })
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
