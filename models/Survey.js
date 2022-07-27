4dc28e278e5e1ee45a1bcf09b7a6c9176a5d603e
Michael Primavera <pastasauce59@gmail.com>
2022-07-27 13:57:19 +0200

    created Recipient model in models directory as subdocument and connected it to Survey model, added more properties to Survey model

--- models/Survey.js
+++ models/Survey.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const { Schema } = mongoose
+const RecipientSchema = require('./Recipient')
 
 const surveySchema = new Schema({
     title: String,
@@ -7,8 +8,14 @@ const surveySchema = new Schema({
     subject: String,
     // Below communicats to mongoose that the 'recipients' property
     // will be an array containing a list of strings.
-    recipients: [String]
-
+    recipients: [RecipientSchema],
+    yes: { type: Number, default: 0 },
+    no: { type: Number, default: 0 },
+    // This property below says that every survey is going to belong to
+    // a particular user. 
+    _user: { type: Schema.Types.ObjectId, ref: 'User' },
+    dateSent: Date,
+    lastResponded: Date
 })
 
 mongoose.model('surveys', surveySchema)
