1276ae238c752b4e72a052b0b9a485cef194f703
Michael Primavera <pastasauce59@gmail.com>
2022-07-29 14:47:00 +0200

    removed acces of axios and console logs

--- client/src/index.js
+++ client/src/index.js
@@ -7,8 +7,6 @@ import reduxThunk from 'redux-thunk'
 
 import App from './components/App'
 import reducers from './reducers'
-import axios from 'axios'
-window.axios = axios
 
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk))
 
@@ -20,6 +18,3 @@ ReactDom.render(
     <Provider store={store}><App /></Provider>,
      document.getElementById('root')
 );
-
-// console.log('STRIPE KEY IS:', process.env.REACT_APP_STRIPE_KEY)
-// console.log('Environment is:', process.env.NODE_ENV)
