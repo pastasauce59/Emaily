4032394c1059cf46de4ff1e19d2299e73dedc671
Michael Primavera <pastasauce59@gmail.com>
2022-07-29 15:49:52 +0200

    refactored surveyTemplate message, added async and await to mailer object in surveyRoutes with catch() for errors, added await to Mailer.js send() response, added redirectDomain to prod.js in config, added back axios to index.js in client src for testing while in development

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -7,10 +7,14 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate')
 const Survey = mongoose.model('surveys')
 
 module.exports = app => {
+    app.get('/api/surveys/thanks', (req, res) => {
+        res.send('Thanks for voting!')
+    })
+
     // request handler can take as many middlerwares as we want but have to be added
     // in the order we want them to execute! i.e. We want to check first
     // that the user is logged in, then check if they have enough credits.
-    app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
+    app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
        const { title, subject, body, recipients } = req.body
 
        const survey = new Survey({
@@ -28,6 +32,17 @@ module.exports = app => {
 
        // Where mailer object will go
        const mailer = new Mailer(survey, surveyTemplate(survey))
-       mailer.send()
+
+       try {
+        await mailer.send()
+        await survey.save()
+        req.user.credits -= 1
+        const user = await req.user.save()
+
+        res.send(user)
+       } catch (err) {
+           res.status(422).send(err)
+       }
+
     })
 }
