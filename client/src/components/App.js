141596569b88f63fb25d1e2bbcce33ca5dace106
Michael Primavera <pastasauce59@gmail.com>
2022-08-01 16:30:02 +0200

    created surveys directory in client to house all survey based components, created SurveyNew component and hooked it up to App.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -8,11 +8,12 @@ import * as actions from '../actions'
 import Header from './Header'
 import Landing from './Landing'
 import Dashboard from './Dashboard'
+import SurveyNew from './surveys/SurveyNew'
 
 // dummy components
 // const Header = () => <h2>Header</h2>
 // const Dashboard = () => <h2>Dashboard</h2>
-const SurveyNew = () => <h2>SurveyNew</h2>
+// const SurveyNew = () => <h2>SurveyNew</h2>
 // const Landing = () => <h2>Landing</h2>
 
 
---
+++ client/src/components/surveys/SurveyNew.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+
+const SurveyNew = () => {
+
+    return (
+        <div>
+            Inside SurveyNew Component
+        </div>
+    )
+}
+
+export default SurveyNew;
