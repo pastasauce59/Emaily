98270a9017c35185039afd12aa2532ba4d290ad2
Michael Primavera <pastasauce59@gmail.com>
2022-08-18 14:52:52 +0200

    imported redux-form to SurveyNew component and connected it to surveyForm  so when user navigates away from this component and the form us unmounted all data entered in form is erased

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,11 +1,14 @@
 // SurveyNew shows: SurveyForm & SurveyFormReview
 
 import React, { useState } from 'react'
+import { useSelector } from 'react-redux'
+import { reduxForm, getFormValues } from 'redux-form'
 import SurveyForm from './SurveyForm';
 import SurveyFormReview from './SurveyFormReview';
 
-const SurveyNew = () => {
+let SurveyNew = () => {
 
+    // const formValues = useSelector(state => getFormValues('forname')(state))
     const [showFormReview, setShowFormReview] = useState(false)
 
     let renderContent = () => {
@@ -28,5 +31,9 @@ const SurveyNew = () => {
         </div>
     )
 }
+
+SurveyNew = reduxForm({
+    form: 'surveyForm'
+})(SurveyNew)
 
 export default SurveyNew;
