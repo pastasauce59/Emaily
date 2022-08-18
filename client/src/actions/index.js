d089e5882bcf5100b4fca7cad43623b4bc28ca97
Michael Primavera <pastasauce59@gmail.com>
2022-08-18 18:14:30 +0200

    expanded on submitSurvey action to handle post request to backend server, send out email, and redirect user to dashboard on client side. Imported useNavigate to SurveyFormReview component and added it to the submitSurvey action

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -15,8 +15,11 @@ export const handleToken = (token) => async dispatch => {
        dispatch({type: FETCH_USER, payload: res.data})
 }
 
-export const submitSurvey = (values) => {
-       return { type: 'submit_survey' }
+export const submitSurvey = (values, navigate) => async dispatch => {
+       const res = await axios.post('/api/surveys', values)
+
+       navigate('/surveys')
+       dispatch({ type: FETCH_USER, payload: res.data })
 }
 
 
--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -3,11 +3,15 @@ import _ from 'lodash'
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import formFields from './formFields'
+// Cannot use withRouter with react-router-dom 6+
+// import { withRouter } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import * as actions from '../../actions'
 
 const SurveyFormReview = ({ onCancel }) => {
     const formValues = useSelector(state => state.form.surveyForm.values)
     const dispatch = useDispatch()
+    const navigate = useNavigate()
 
     const reviewFields = _.map(formFields, ({ name, label }) => {
         return (
@@ -31,7 +35,7 @@ const SurveyFormReview = ({ onCancel }) => {
                 Back
             </button>
             <button 
-                onClick={ () => dispatch(actions.submitSurvey(formValues)) }
+                onClick={ () => dispatch(actions.submitSurvey(formValues, navigate)) }
                 className='green white-text btn-flat right'>
                 Send Survey
                 <i className='material-icons right'>email</i>
