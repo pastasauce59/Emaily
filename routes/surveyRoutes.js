ba617ede96b6b9c63f61976046eecca68a167951
Michael Primavera <pastasauce59@gmail.com>
2022-08-23 00:00:03 +0200

    sizable refactoring of code to the events object with the help of lodash chain helper function to simplify and make more readable

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -18,27 +18,20 @@ module.exports = app => {
     app.post('/api/surveys/webhooks', (req, res) => {
         // console.log(req.body)
         // res.send({})
+        const p = new Path('/api/surveys/:surveyId/:choice')
+        
+       const events =  _.chain(req.body)
+            .map(({ email, url}) => {
+                const match = p.test(new URL(url).pathname)
+                if (match) {
+                    return { email, surveyId: match.surveyId, choice: match.choice }
+                }
+            })
+            .compact()
+            .uniqBy('email', 'surveyId')
+            .value()
 
-        const events = _.map(req.body, ({ email, url}) => {
-            const pathname = new URL(url).pathname
-            const p = new Path('/api/surveys/:surveyId/:choice')
-            // console.log(p.test(pathname))
-            const match = p.test(pathname)
-            if (match) {
-                return { email, surveyId: match.surveyId, choice: match.choice }
-            }
-        })
-
-        // console.log(events)
-
-        // compact() is another function fomr the lodash library
-        // it takes an array and removes any elements that are undefined
-        const compactEvents = _.compact(events)
-
-        // uniqBy is another function from lodash library, if there are any duplictes they are removed
-        const uniqueEvents = _.uniqBy(compactEvents, 'email', 'surveyId')
-
-        console.log(uniqueEvents)
+        console.log(events)
 
         // response to request to let sendgrid know everything is ok and stop sending duplicate responses
         res.send({})
